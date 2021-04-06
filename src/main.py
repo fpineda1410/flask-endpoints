c91a06dfb6e9a9c6ec159238343266798eb5f850
fpineda1410 <fpineda147@outlook.com>
2021-04-06 14:41:34 +0000

    auth changes

--- src/main.py
+++ src/main.py
@@ -8,19 +8,45 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Character,Planet,FavoritePlanet,FavoriteCharacter
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import current_user
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+import requests
+ #ste men
 #from models import Person
+ 
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
+jwt = JWTManager(app)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+# insert character and planet data into the SQL databases
+
+
+
+
+
 # Handle/serialize errors like a JSON object
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
@@ -30,16 +56,62 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/create-account', methods=['POST'])
+def create_account():
+    body=request.get_json()
+
+    if body is None:
+        return "The request body is null", 400
+    if 'username' not in body:
+        return "Empty username", 400
+    if 'email' not in body:
+        return "Empty email", 400
+    if 'password' not in body:
+        return "Empty password", 400
+
+    user=User()
+    user.username=body['username']
+    user.email=body['email']
+    user.password=body['password']
+    user.is_active=True
+
+    db.session.add(user)
+    db.session.commit()
+
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Added user"
     }
 
     return jsonify(response_body), 200
 
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(username=username).one_or_none()
+    
+    if not user or not user.check_password(password):
+        return jsonify("Wrong username or password"), 401
+    
+    access_token = create_access_token(identity=user)
+    return jsonify(access_token=access_token)
+
+@app.route("/user_identity", methods=["GET"])
+@jwt_required()
+def protected():
+    # We can now access our sqlalchemy User object via `current_user`.
+    return jsonify(
+        id=current_user.id,
+        full_name=current_user.email,
+        username=current_user.username,
+    )
+
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
+    #inyeccion a la base de datos 
+    #
+    # data_inyection()
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,12 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import safe_str_cmp
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=True)
+    favorites_planets= db.relationship('FavoritePlanet', backref='user', lazy=True)
+    favorites_characters= db.relationship('FavoriteCharacter', backref='user', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +21,71 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+    def check_password(self, password):
+        return safe_str_cmp(password, self.password)
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_day = db.Column(db.String(100), nullable=False)
+    gender = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    skin_color = db.Column(db.String(100), nullable=False)
+    hair_color = db.Column(db.String(100), nullable=False)
+    eye_color = db.Column(db.String(100), nullable=False)
+    favorite_id = db.relationship('FavoriteCharacter', backref='character', lazy=True)
+    
+    def __repr__(self):
+        return '<Character %s>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_day": self.birth_day,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            # do not serialize the password, its a security breach
+        }
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    climate = db.Column(db.String(100), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+    terrain = db.Column(db.String(100), nullable=False)
+    rotation_period = db.Column(db.Integer, nullable=False)
+    orbital_period = db.Column(db.Integer, nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    favorite_id = db.relationship('FavoritePlanet', backref='planet', lazy=True)
+    
+    def __repr__(self):
+        return '<Character %s>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "population": self.population,
+            "terrain": self.terrain,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            # do not serialize the password, its a security breach
+        }
+
+class FavoritePlanet (db.Model): # favorite_planet
+    id = db.Column(db.Integer, primary_key=True)
+    planet_id= db.Column(db.Integer,db.ForeignKey('planet.id'),nullable=False)
+    user_id= db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+
+class FavoriteCharacter (db.Model): ## favorite_character
+    id = db.Column(db.Integer, primary_key=True)
+    character_id= db.Column(db.Integer,db.ForeignKey('character.id'),nullable=False)
+    user_id= db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    
